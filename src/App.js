5f4e396ffb8e55b4786bbf5e7c2bde610ff5f713
John <johnleavell@gmail.com>
2020-12-22 14:13:31 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      movieTitle: '',
+    }
+  }
+
+  searchMovie = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  submitMovie = e => {
+    e.preventDefault();
+
+  }
+
+  render(){
+    return (
+      <div>
+      <form onSubmit={this.searchMovie}>
+        <label htmlFor='movieTitle'>
+          <strong>Search for Movie by Title:</strong>
+
+            <input>
+              <input>
+
+              </input>
+            </input>
+        </label>
+      </form>
+    
+      </div>
+    );
+  }
+  
 }
 
 export default App;
